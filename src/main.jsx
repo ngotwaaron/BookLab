ec1a848a55098bd818bd224c24c440db001f9c67
ngotwaaron <ngotwaaron@gmail.com>
2024-05-08 15:41:47 +0300

    importing all components to main app

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Books from "./Components/Books/Books";
+import BookDetail from "./Components/Books/BookDetail";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      {/* <Route path="login" element={<Login />} />
+      <Route path="signup" element={<SignUp />} /> */}
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route exact path="books" element={<Books />} />
+      <Route path="bookDetail/:id" element={<BookDetail />} />
+    </Route>
+  )
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
